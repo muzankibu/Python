630bcc46acfc0e572f69d7e0316a1517563f814c
muzankibu <muzankibu977@gmail.com>
2024-12-01 22:48:21 +0600

    updated codefiles

--- Built-In Function/Lambda/code3.py
+++ Built-In Function/Lambda/code3.py
@@ -1,3 +1,5 @@
 
 greet_user = lambda name : print('Hey there,'+ name+"!")
-greet_user('Showrav')
+greet_user('Soleman Hossain Showrav')
+
+print(lambda name : print('Hey there,'+ name+"!")('Soleman Hossain Showrav'))
